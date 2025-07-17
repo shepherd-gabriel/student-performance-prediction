a5da0aa62607f6b361e393f68b28fde48fea6baa
shepherd-gabriel <shepherdgabrielglobal@gmail.com>
2025-07-17 07:48:17 +0100

    Add files via upload

---
+++ installing liberies code.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install numpy')
+get_ipython().system('pip install panda')
+get_ipython().system('pip install matplotlib')
+get_ipython().system('pip install seabon')
+get_ipython().system('pip install scikit-learn')
+get_ipython().system('pip install scipy')
+
+
+# In[25]:
+
+
+get_ipython().system('pip install pandas')
+
+
+# In[4]:
+
+
+get_ipython().system('pip install seaborn')
+get_ipython().system('pip install matplotlib')
+
+
+# In[5]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
+# In[7]:
+
+
+get_ipython().system('pip install xgboost')
+get_ipython().system('pip install flask')
+
+
+# In[ ]:
+
+
+
+
